99d38674809cd5b00c39174b93ef4ef90e7bb4d6
jd6286 <93331084+jd6286@users.noreply.github.com>
2024-09-22 17:02:30 +0900

    Vending Machine Config Fix
    1. 송신,수신 버퍼를 동적으로 할당해서 스택 메모리 확보
    2. ioc에서 RTOS의 Config를 수정해서 code generation시 오류가 발생하지 않도록 설정

--- In_Progress/Vending_Machine/Core/Inc/FreeRTOSConfig.h
+++ In_Progress/Vending_Machine/Core/Inc/FreeRTOSConfig.h
@@ -54,7 +54,7 @@
 #endif
 #define configENABLE_FPU                         0
 #define configENABLE_MPU                         0
-#define configCHECK_FOR_STACK_OVERFLOW 2
+
 #define configUSE_PREEMPTION                     1
 #define configSUPPORT_STATIC_ALLOCATION          1
 #define configSUPPORT_DYNAMIC_ALLOCATION         1
@@ -69,6 +69,7 @@
 #define configUSE_16_BIT_TICKS                   0
 #define configUSE_MUTEXES                        1
 #define configQUEUE_REGISTRY_SIZE                8
+#define configCHECK_FOR_STACK_OVERFLOW           2
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION  1
 /* USER CODE BEGIN MESSAGE_BUFFER_LENGTH_TYPE */
 /* Defaults to size_t for backward compatibility, but can be changed
@@ -94,7 +95,6 @@ to exclude the API function. */
 #define INCLUDE_vTaskDelay                   1
 #define INCLUDE_xTaskGetSchedulerState       1
 #define INCLUDE_uxTaskGetStackHighWaterMark  1
-
 
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
